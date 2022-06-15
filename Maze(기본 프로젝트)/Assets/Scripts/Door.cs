333174e316d49ce2e47ed8cddef7f0523965527a
asdk1001 <96044657+asdk1001@users.noreply.github.com>
2022-06-15 13:36:39 +0900

    Base

---
+++ Maze(기본 프로젝트)/Assets/Scripts/Door.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Door : MonoBehaviour
+{
+    protected bool isClosed;
+    public abstract void OpenOrClose(GameObject player);
+}
