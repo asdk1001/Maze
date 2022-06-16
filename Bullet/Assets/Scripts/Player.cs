d725da4b4e226dc4a431a89661f15a17fff0d9f6
asdk1001 <96044657+asdk1001@users.noreply.github.com>
2022-06-16 20:43:36 +0900

    Bullet 추가

---
+++ Bullet/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletobj;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            //Bullet bullet = Instantiate(bulletobj).GetComponent<Bullet>();
+            Bullet bullet = ObjectPool.GetObject();            
+            bullet.Shoot();
+        }
+    }
+}
